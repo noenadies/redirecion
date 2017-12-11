6040201d200c53f519e61d81c11466f1ae7d2198
noenadies <33671023+noenadies@users.noreply.github.com>
2017-12-11 11:51:22 -0500

    Update login.js

--- login.js
+++ login.js
@@ -3,9 +3,9 @@ var attempt = 3; // Variable to count number of attempts.
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username == "Formget" && password == "formget#123"){
+if ( username == "a" && password == "1"){
 alert ("Login successfully");
-window.location ="https://noenadies.github.io/titanic/";// "dos.html"; // Redirecting to other page.
+window.location ="https://noenadies.github.io/titanic/";//// "dos.html"; // Redirecting to other page.
 return false;
 }
 else{
