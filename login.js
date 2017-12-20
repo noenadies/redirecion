9e9103efedbbd1036017cbb288468f92aafecb74
noenadies <33671023+noenadies@users.noreply.github.com>
2017-12-20 16:09:21 -0500

    Update login.js

--- login.js
+++ login.js
@@ -6,7 +6,7 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "a" && password == "1"){
 alert ("Login successfully");
-window.location ="https://noenadies.github.io/titanic/";// "dos.html"; // Redirecting to other page.
+window.location ="https://sites.google.com/view/cal3d/cal3d";//"https://noenadies.github.io/titanic/";// "dos.html"; // Redirecting to other page.
 return false;
 }
 else{
